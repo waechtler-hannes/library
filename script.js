c4dc5142ec10ce0a75f747c76984072d1e564950
Hannes <142382297+waechtler-hannes@users.noreply.github.com>
2024-12-20 09:38:42 +0100

    Add books from array to website
    Changed layout of book-content.
    Changed style of delete- and addBook-buttons.
    Added book-content with javascript from the before created array.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const myLibrary = [];
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolikien", 295, false);
-addBookToLibrary("Der Name der Rose", "Umberto Eco", 500, true);
-addBookToLibrary("Der kleine Prinz", "Antoine de Saint-Exupéry", 96, false);
-addBookToLibrary("Harry Potter und der Stein der Weisen", "J.K. Rowling", 309, false);
+addBookToLibrary("The Name of the Rose", "Umberto Eco", 500, true);
+addBookToLibrary("The Little Prince", "Antoine de Saint-Exupéry", 96, false);
+addBookToLibrary("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 309, false);
 addBookToLibrary("1984", "George Orwell", 328, true);
-addBookToLibrary("Stolz und Vorurteil", "Jane Austen", 432, false);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 432, false);
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -21,10 +21,33 @@ function addBookToLibrary(title, author, pages, read) {
 function displayBooks(array) {
     const libraryDisplay = document.querySelector(".main-content")
     array.forEach(book => {
+        // Create needed Elements
         const bookElement = document.createElement("div");
-        bookElement.textContent = book.title;
+        const title = document.createElement("h3");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const deleteButton = document.createElement("div");
+        const button = document.createElement("button");
+        // Add classes to Elements
+        bookElement.classList.add("book");
+        title.classList.add("title");
+        author.classList.add("author");
+        pages.classList.add("pages");
+        deleteButton.classList.add("delete-button");
+        button.classList.add("button");
+        // Add Text-Content
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages + " pages";
+        button.textContent = "Delete";
+        // Append Elements
         libraryDisplay.appendChild(bookElement);
+        bookElement.appendChild(title);
+        bookElement.appendChild(author);
+        bookElement.appendChild(pages);
+        bookElement.appendChild(deleteButton);
+        deleteButton.appendChild(button);
     });
 }
 
-// displayBooks(myLibrary);
+displayBooks(myLibrary);
